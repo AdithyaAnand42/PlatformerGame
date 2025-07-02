f59be0ef77a70708dc5cd36eee79dba5c2641fe1
2x2latMaine <124173816+2x2latMaine@users.noreply.github.com>
2025-07-02 00:07:16 -0400

    Revert "Merge branch 'main' of https://github.com/AdithyaAnand42/PlatformerGame"
    This reverts commit 928fac84ae878590d6919a79fc3daaac58474d9d, reversing
    changes made to 8f951cfe655d7c9efbaac1d5abfa5bb7bb6ded9e.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,8 +2,8 @@ extends Node
 
 var score = 0
 
-@onready var score_label: Label = $ScoreLabel
+@onready var score_lable: Label = $ScoreLable
 
 func add_point():
 	score += 1
-	score_label.text = "You collected " + str(score) + " coins"
+	score_lable.text = "You collected " + str(score) + " coins"
